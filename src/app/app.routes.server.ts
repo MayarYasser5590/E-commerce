fa6866a5135ea346e062f8a89baa5270e265c84a
MayarYasser5590 <MayarYasser5590@gmail.com>
2025-03-01 04:47:05 +0200

    improve

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,13 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import path from 'path';
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'details/:id',
+    renderMode: RenderMode.Server
   }
 ];
