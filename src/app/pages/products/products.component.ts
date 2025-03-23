f20af9ca4c12ccbeb2f892acaf7285e338176e85
MayarYasser5590 <MayarYasser5590@gmail.com>
2025-03-23 06:35:48 +0200

    improve

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { ProductsService } from '../../core/services/products/products.service';
 import { Subscription } from 'rxjs';
 import { IProduct } from '../../shared/interfaces/iproduct';
@@ -42,6 +42,12 @@ getProductsData():void{
    })
  }
 
+ @ViewChild('myElement') ele !: ElementRef;
+ iconColor():void{
+  this.ele.nativeElement.classList.remove('text-gray-400');
+  this.ele.nativeElement.classList.add('text-red-600');
+ }
+
  addToCart(id:string):void{
   this.isLoading = true;
   this.cartSubscribe = this.cartService.addProductToCard(id).subscribe({
@@ -56,10 +62,10 @@ getProductsData():void{
 }
 
 addToWislist(id:string):void{
+  this.iconColor();
   this.wishlistSubscribe = this.wishlistService.addProductToWishlist(id).subscribe({
    next:(res)=>{
      this.toastr.success(res.message, 'Hi!');
-     this.isActive = !this.isActive;
    },
    error:(err)=>{
      console.log(err);
